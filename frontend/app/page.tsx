d45f9af1aca270c3562b8f99023412d436196a8b
andrewchimney <93691894+andrewchimney@users.noreply.github.com>
2026-01-21 21:52:00 -0800

    more infrasctructure

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -4,7 +4,20 @@ export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
+        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
+          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+            SynthGPT
+          </h1>
+          <Image
+          className="dark:invert"
+          src="/robot-black.svg"
+          alt="SynthGPT logo"
+          width={100}
+          height={20}
+          priority
+        />
+        </div>
+        {/* <Image
           className="dark:invert"
           src="/next.svg"
           alt="Next.js logo"
@@ -58,7 +71,7 @@ export default function Home() {
           >
             Documentation
           </a>
-        </div>
+        </div> */}
       </main>
     </div>
   );
